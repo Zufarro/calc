2e68cd5032072f67a8dfcb0f7d3ca7e213e35242
Zufarro <zufar-galimzyanov@yandex.ru>
2019-03-04 20:48:46 +0300

    added returning of methods

--- calc.py
+++ calc.py
@@ -3,16 +3,18 @@ class Calculator ():
         self.all_result_history = {}
         self.temp_result = []
     def add (self, x, y):
-        pass
+        return x + y
     def sub (self, x, y):
-        pass
+        return x - y
     def mul (self, x, y):
-        pass
+        return x * y
     def div (self, x, y):
-        pass
+        return x /y
     def clear_result (self):
         pass
     def moving_result_in_temp(self):
         pass
     def clear_temp_result(self):
         pass
+
+my_calc = Calculator()
