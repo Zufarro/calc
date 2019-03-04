94725ea1438faa6ac9e0b5011cb227dedb336b45
Zufarro <zufar-galimzyanov@yandex.ru>
2019-03-04 19:42:37 +0300

    add_model_calc

---
+++ calc.py
@@ -0,0 +1,18 @@
+class Calculator ():
+    def __init__(self):
+        self.all_result_history = {}
+        self.temp_result = []
+    def add (self, x, y):
+        pass
+    def sub (self, x, y):
+        pass
+    def mul (self, x, y):
+        pass
+    def div (self, x, y):
+        pass
+    def clear_result (self):
+        pass
+    def moving_result_in_temp(self):
+        pass
+    def clear_temp_result(self):
+        pass
