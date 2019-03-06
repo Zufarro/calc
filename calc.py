851f88557661c6bf991f35b99d3660c77badad38
Zufarro <zufar-galimzyanov@yandex.ru>
2019-03-06 18:58:34 +0300

    first implementation

--- calc.py
+++ calc.py
@@ -1,35 +1,72 @@
-class Calculator ():
+class Calculator:
     def __init__(self):
         self.all_result_history = {}
+        self.auto_temp_result = .0
         self.temp_result = .0
 
-    def add (self, x, y):
+    def add(self, x, y):
         if y is not None:
             result = x + y
             self.all_result_history[str(x) + " + " + str(y) + " = "] = result
-            self.temp_result = result
+            self.auto_temp_result = result
             return result
-        elif y is None:
+        else:
             if self.temp_result != 0:
                 result = self.temp_result + x
                 return result
+                self.temp_result = .0
             else:
                 return "failing"
 
-    def sub (self, x, y):
-        return x - y
+    def sub(self, x, y):
+        if y is not None:
+            result = x - y
+            self.all_result_history[str(x) + " - " - str(y) + " = "] = result
+            self.auto_temp_result = result
+            return result
+        else:
+            if self.temp_result != 0:
+                result = self.temp_result - x
+                return result
+                self.temp_result = .0
+            else:
+                return "failing"
 
-    def mul (self, x, y):
-        return x * y
+    def mul(self, x, y):
+        if y is not None:
+            result = x * y
+            self.all_result_history[str(x) + " * " + str(y) + " = "] = result
+            self.auto_temp_result = result
+            return result
+        else:
+            if self.temp_result != 0:
+                result = self.temp_result * x
+                return result
+                self.temp_result = .0
+            else:
+                return "failing"
 
-    def div (self, x, y):
-        return x /y
+    def div(self, x, y):
+        if y is not None and y != 0:
+            result = x / y
+            self.all_result_history[str(x) + " / " + str(y) + " = "] = result
+            self.auto_temp_result = result
+            return result
+        elif y is None:
+            if self.temp_result != 0:
+                result = self.temp_result / x
+                return result
+                self.temp_result = .0
+            else:
+                return "failing"
+        else:
+            return "failing"
 
-    def clear_result (self):
-        pass
+    def clear_all_result_history(self):
+        self.all_result_history = {}
 
-    def moving_result_in_temp(self):
-        pass
+    def last_result_in_temp(self):
+        self.temp_result = self.auto_temp_result
 
     def clear_temp_result(self):
-        pass
+        self.temp_result = .0
--- using.py
+++ using.py
@@ -5,6 +5,11 @@ my_calc = Calculator()
 
 print(my_calc.add(5,4))
 print(my_calc.add(4,6))
-print (my_calc.all_result_history)
+print(my_calc.all_result_history)
+print(my_calc.auto_temp_result)
+my_calc.last_result_in_temp()
 print (my_calc.temp_result)
 print(my_calc.add(4,None))
+
+print(my_calc.mul(5,4))
+print(my_calc.div(7,6))
