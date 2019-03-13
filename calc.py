e2cc978f00469866d942cc4af87f009ee74d14f9
Zufarro <zufar-galimzyanov@yandex.ru>
2019-03-13 17:10:16 +0300

    bugs fixed

--- calc.py
+++ calc.py
@@ -1,75 +1,75 @@
 class Calculator:
     def __init__(self):
-        self.steps_memory = {}
-        self.auto_memory = .0
-        self.memory = .0
+        self.history = []
+        self.result = None
+        self.memory = None
+
+    def save(self,result,x,y,op):
+        self.history.append(str(x)+op+str(y)+"="+str(result))
+        self.result = result
 
     def add(self, x, y):
         if y is not None:
             result = x + y
-            self.steps_memory[str(x) + " + " + str(y) + " = "] = result
-            self.auto_memory = result
+            self.save(result,x,y,"+")
             return result
         else:
-            if self.memory != 0:
+            if self.memory:
                 result = self.memory + x
+                self.memory = None
                 return result
-                self.memory = .0
             else:
                 return "failing"
 
     def sub(self, x, y):
         if y is not None:
             result = x - y
-            self.steps_memory[str(x) + " - " + str(y) + " = "] = result
-            self.auto_memory = result
+            self.save(result,x,y,"-")
             return result
         else:
-            if self.memory != 0:
+            if self.memory:
                 result = self.memory - x
+                self.memory = None
                 return result
-                self.memory = .0
             else:
                 return "failing"
 
     def mul(self, x, y):
         if y is not None:
             result = x * y
-            self.steps_memory[str(x) + " * " + str(y) + " = "] = result
-            self.auto_memory = result
+            self.save(result,x,y,"*")
             return result
         else:
-            if self.memory != 0:
+            if self.memory:
                 result = self.memory * x
+                self.memory = None
                 return result
-                self.memory = .0
             else:
                 return "failing"
 
     def div(self, x, y):
         if y is not None and y != 0:
             result = x / y
-            self.steps_memory[str(x) + " / " + str(y) + " = "] = result
-            self.auto_memory = result
+            self.save(result,x,y,"/")
             return result
         elif y is None:
-            if self.memory != 0:
+            if self.memory:
                 result = self.memory / x
+                self.memory = None
                 return result
-                self.memory = .0
             else:
                 return "failing"
         else:
             return "failing"
 
-    def clear_steps_memory(self):
-        self.steps_memory = {}
+    def clear_history(self):
+        self.history = []
 
     def memory_save(self):
-        self.memory = self.auto_memory
+        self.memory = self.result
 
     def clear_memory(self):
-        self.memory = .0
+        self.memory = None
 
 
 from math import sqrt as sq
--- using.py
+++ using.py
@@ -5,8 +5,8 @@ my_calc = Calculator()
 
 print(my_calc.add(5,4))
 print(my_calc.add(4,6))
-print(my_calc.steps_memory)
-print(my_calc.auto_memory)
+print(my_calc.history)
+print(my_calc.result)
 my_calc.memory_save()
 print (my_calc.memory)
 print(my_calc.add(4,None))
@@ -18,9 +18,9 @@ print(my_calc.sub(10,6))
 print(my_calc.div(7,0))
 
 
-print(my_calc.steps_memory)
-my_calc.clear_steps_memory()
-print(my_calc.steps_memory)
+print(my_calc.history)
+my_calc.clear_history()
+print(my_calc.history)
 
 my_calc.memory_save()
 print (my_calc.memory, " add")
